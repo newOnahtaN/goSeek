2edfb96be71bd5e18f16d5137e75e7016c4fb277
Nathan Owen <ncowen@email.wm.edu>
2015-04-11 08:52:22 -0400

    Added Sound effect

--- goSeek/goSeek/ViewController.m
+++ goSeek/goSeek/ViewController.m
@@ -30,6 +30,7 @@
     EventSource *sourceMarco = [EventSource eventSourceWithURL:serverURL];
     [sourceMarco addEventListener:@"MARCO" handler:^(Event *e) {
         NSLog(@"%@: %@", e.event, e.data);
+        AudioServicesPlaySystemSound (1003);
     }];
     
     EventSource *sourceClose = [EventSource eventSourceWithURL:serverURL];
